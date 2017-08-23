4d86c39634eabe019698013924b2617ec47787a7
Aleksander Chromik <achromik@gmail.com>
2017-08-23 10:50:45 +0200

    Excercise 8.3

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var name = prompt('Hi, my name is Alex! \nWhat is your name?');
+alert('Hello, ' + name + '! Welcome to Excercise 8.3! Nice to see you again!');
+console.log('Hello, ' + name + '! Welcome to Excercise 8.3! Nice to see you again!');
